873a91d1a961e39b5ea4a612a4a7b5e0c3b34548
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-09 17:50:36 +0100

    add const uppercase

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -8,7 +8,7 @@ public class CalcolaBiglietto {
 		
 		Scanner in = new Scanner(System.in);
 		
-		final double price = 0.21;
+		final double PRICE = 0.21;
 		
 		System.out.println("quanti chilometri vuoi percorrere?: ");
 		String km1 = in.nextLine();
@@ -18,11 +18,11 @@ public class CalcolaBiglietto {
 		String age1 = in.nextLine();
 		int age2 = Integer.valueOf(age1);
 		
-		double finalPrice = km2 * price;
-		final double discount20 = finalPrice * 0.20;
-		final double discount40 = finalPrice * 0.40;
-		double finalPrice2 = finalPrice - discount20;
-		double finalPrice3 = finalPrice - discount40;
+		double finalPrice = km2 * PRICE;
+		final double DISCOUNT20 = finalPrice * 0.20;
+		final double DISCOUNT40 = finalPrice * 0.40;
+		double finalPrice2 = finalPrice - DISCOUNT20;
+		double finalPrice3 = finalPrice - DISCOUNT40;
 		
 		if(age2 <= 18) {
 			System.out.println("il prezzo del biglietto è: " + finalPrice2);
