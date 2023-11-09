05fcfec1c89586bea5a51ccbf5e734e0e3bad9a7
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-09 18:40:26 +0100

    refactoring

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -22,20 +22,22 @@ public class CalcolaBiglietto {
 		double finalPrice = km2 * PRICE;
 		final double DISCOUNT20 = finalPrice * 0.20;
 		final double DISCOUNT40 = finalPrice * 0.40;
-		double PriceDiscount20 = finalPrice - DISCOUNT20;
-	    String prezzoFormattato20 = String.format("%.2f", PriceDiscount20);
+		double priceWith20 = finalPrice - DISCOUNT20;
+	    String finalPriceWith20 = String.format("%.2f", priceWith20);
 	
-		double PriceDiscount40 = finalPrice - DISCOUNT40;
-		String prezzoFormattato40 = String.format("%.2f", PriceDiscount40);
+		double priceWith40 = finalPrice - DISCOUNT40;
+		String finalPriceWith40 = String.format("%.2f", priceWith40);
+		
+		String formattedPriceWithoutDiscount = String.format("%.2f", finalPrice);
 		
 		if(age2 <= 18) {
-			System.out.println("il prezzo del biglietto è: " + prezzoFormattato20);
+			System.out.println("il prezzo del biglietto è: " + finalPriceWith20);
 		}
 		else if(age2 >= 65) {
-			System.out.println("il prezzo del biglietto è: " + prezzoFormattato40);
+			System.out.println("il prezzo del biglietto è: " + finalPriceWith40);
 		}
 		else {
-			System.out.println("il prezzo del biglietto è: " + finalPrice);
+			System.out.println("il prezzo del biglietto è: " + formattedPriceWithoutDiscount);
 		}
 		
 		in.close();
